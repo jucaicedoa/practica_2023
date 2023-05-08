ca7836e5591e6ddff51181ff1889c71ba4176367
Juan David Caicedo Aponte <jucaicedoa@unal.edu.co>
2023-05-07 22:38:21 -0500

    agrego ejemplos iniciales arreglos

---
+++ practica_java/arreglos.java
@@ -0,0 +1,23 @@
+/**
+ * arreglos
+ */
+public class arreglos {
+
+    public static void main(String[] args) {
+        String letras[]= new String[3];
+    // String vocales[] = { "a","e","i","o","u"};
+        letras[0]= "a";
+        letras[1]= "b";
+        letras[2]= "c";
+        System.out.println(letras[0]);
+        System.out.println(letras[1]);
+        System.out.println(letras[2]);
+    /*
+     * System.out.println(vocales[0]);
+     * System.out.println(vocales[1]);
+     * System.out.println(vocales[2]);
+     * System.out.println(vocales[3]);
+     * System.out.println(vocales[4]);
+     */
+    }
+}
