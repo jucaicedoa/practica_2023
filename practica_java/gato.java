1ff3eebfb3a4a254931bc4286a37ce159b72d9ce
Juan David Caicedo Aponte <jucaicedoa@unal.edu.co>
2023-05-08 23:40:42 -0500

    agrego clase gato

---
+++ practica_java/gato.java
@@ -0,0 +1,14 @@
+/**
+ * gato
+ */
+public class gato {
+
+    String nombre;
+    int edad;
+    String color;
+    gato(String nombre, int edad, String color){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.color=color;
+    }
+}
