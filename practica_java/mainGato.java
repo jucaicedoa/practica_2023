66dc4fc0d3a06a6a175398418ecc8cf907cdd5f4
Juan David Caicedo Aponte <jucaicedoa@unal.edu.co>
2023-05-08 23:41:12 -0500

    complemento clases

---
+++ practica_java/mainGato.java
@@ -0,0 +1,10 @@
+public class mainGato {
+    public static void main(String[] args) {
+        gato Venus = new gato("Venus",2,"Blanca con manchitas");
+    
+       System.out.println("El nombre del gato es: "+Venus.nombre); 
+
+    }
+
+   
+}
