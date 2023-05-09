6dfca59a9d9a7e278d24f2faafac0f36781aa41d
Juan David Caicedo Aponte <jucaicedoa@unal.edu.co>
2023-05-08 23:39:38 -0500

    agrego metodos sobre cadenas

---
+++ practica_java/metodos_cadenas.java
@@ -0,0 +1,12 @@
+public class metodos_cadenas {
+    public static void main(String[] args) {
+
+        String cadena = "Repositorio jucaicedoa"; // Un String está compuesto por elementos de tipo char los cuales tienen una posición en el String que inicia en 0
+        System.out.println(cadena.length()); // Tamaño de la cadena
+        // System.out.println(cadena.charAt(2));
+        char parte = cadena.charAt(2);
+        System.out.println(parte);
+        System.out.println(cadena.indexOf("o")); // primera conincidencia desde el comienzo
+        System.out.println(cadena.lastIndexOf("e")); // primera conincidencia desde el final
+    }
+}
