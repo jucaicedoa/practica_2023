2827e9b77e1b7a63a144b7e80d3ef19251d72437
Juan David Caicedo Aponte <jucaicedoa@unal.edu.co>
2023-05-09 23:59:36 -0500

    agrego codigo ejemplo a gato

--- practica_java/gato.java
+++ practica_java/gato.java
@@ -2,13 +2,43 @@
  * gato
  */
 public class gato {
-
-    String nombre;
-    int edad;
+    // Atributos
+    private String nombre;
+    public int edad;
     String color;
-    gato(String nombre, int edad, String color){
-        this.nombre=nombre;
-        this.edad=edad;
-        this.color=color;
+
+    public gato(String nombre, int edad, String color) { // Constructor siempre tiene el nombre de la clase
+        this.nombre = nombre;
+        this.edad = edad;
+        this.color = color;
+    }
+
+    // Métodos
+    void lamiendo() {
+        System.out.println("El gato se está lamiendo");
+    }
+
+    void comiendo() {
+        System.out.println("El gato está comiendo");
+    }
+
+    void dormir() {
+        System.out.println("El gato se encuentra dormido");
+    }
+
+    String agregado() {// Solo es un ejemplo puesto que como modelo el nombre María no me serviría para
+                       // generalizar
+        String nombreAgregado = this.nombre + " María";
+        return nombreAgregado;
+    }
+
+    // Getter
+    String getNombre() { // Se puede acceder a un atributo privado utilizando el método get
+        return this.nombre;
+    }
+
+    // Setter
+    void setNombre(String nombre) {// Se puede modificar un atributo privado utilizando el método set
+        this.nombre = nombre;
     }
 }
