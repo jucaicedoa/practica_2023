ce22a640d50d3d3abd351e5a2fcf3fdc7cb798bd
Juan David Caicedo Aponte <jucaicedoa@unal.edu.co>
2023-05-10 00:00:03 -0500

    agregar codigo main gato

--- practica_java/mainGato.java
+++ practica_java/mainGato.java
@@ -1,10 +1,12 @@
 public class mainGato {
     public static void main(String[] args) {
-        gato Venus = new gato("Venus",2,"Blanca con manchitas");
-    
-       System.out.println("El nombre del gato es: "+Venus.nombre); 
-
+        gato Venus = new gato("Venus", 2, "Blanca con manchitas");
+        System.out.println("El nombre del gato es: " + Venus.getNombre());
+        Venus.lamiendo();
+        Venus.comiendo();
+        Venus.dormir();
+        String nombres = Venus.agregado();
+        System.out.println(nombres);
     }
 
-   
 }
