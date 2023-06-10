89f749857bdc054d6030b4c65da1bdcc7bc094fe
Juan David Caicedo Aponte <jucaicedoa@unal.edu.co>
2023-06-09 19:16:11 -0500

    main sobreCarga

---
+++ practica_java/mainSobreCarga.java
@@ -0,0 +1,16 @@
+public class mainSobreCarga {
+    public static void main(String[] args) {
+
+
+        sobreCargaOpera ejemplo = new sobreCargaOpera();
+        int resultado1 = ejemplo.suma(2, 3);
+        System.out.println("El resultado de la suma es: " + resultado1);
+
+        float resultado2 = ejemplo.suma(3.4f, 2.3f);
+        System.out.println("El resultado de la suma es: " + resultado2);
+
+        int resultado3 = ejemplo.suma(12,3,4,5,6,7);
+         System.out.println("El resultado de la suma es: " + resultado3);
+    }
+
+}
