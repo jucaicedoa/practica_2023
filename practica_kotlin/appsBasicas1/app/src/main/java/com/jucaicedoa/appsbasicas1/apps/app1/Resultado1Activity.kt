d6e2337199682b74adf30b38aed926ebfeefd514
Juan David Caicedo Aponte <jucaicedoa@unal.edu.co>
2023-06-16 16:11:34 -0500

    cambio nombre archivo

--- practica_kotlin/appsBasicas1/app/src/main/java/com/jucaicedoa/appsbasicas1/apps/app1/Resultado1Activity.kt
+++ practica_kotlin/appsBasicas1/app/src/main/java/com/jucaicedoa/appsbasicas1/apps/app1/Resultado1Activity.kt
@@ -8,7 +8,7 @@ import com.jucaicedoa.appsbasicas1.R
 class Resultado1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        setContentView(R.layout.activity_resultado1)
         val vistaResult = findViewById<TextView>(R.id.vistaResult)
         val nombre: String = intent.extras?.getString("EXTRA_NAME").orEmpty()
         vistaResult.text = "Hola $nombre"
