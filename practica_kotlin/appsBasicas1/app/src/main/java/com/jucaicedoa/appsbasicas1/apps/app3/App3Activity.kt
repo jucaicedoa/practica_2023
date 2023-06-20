2b73846c91d77e7c32132a10ce36ad0f39263f3f
Juan David Caicedo Aponte <jucaicedoa@unal.edu.co>
2023-06-20 10:57:38 -0500

    inicio y avance app básica 3

---
+++ practica_kotlin/appsBasicas1/app/src/main/java/com/jucaicedoa/appsbasicas1/apps/app3/App3Activity.kt
@@ -0,0 +1,12 @@
+package com.jucaicedoa.appsbasicas1.apps.app3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jucaicedoa.appsbasicas1.R
+
+class App3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app3)
+    }
+}
