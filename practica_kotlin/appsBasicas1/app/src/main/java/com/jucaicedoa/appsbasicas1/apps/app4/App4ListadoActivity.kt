5ddeecd2c041497632ad3b7cee7c85db0c966097
Juan David Caicedo Aponte <jucaicedoa@unal.edu.co>
2023-06-28 09:19:37 -0500

    Inicio app4

---
+++ practica_kotlin/appsBasicas1/app/src/main/java/com/jucaicedoa/appsbasicas1/apps/app4/App4ListadoActivity.kt
@@ -0,0 +1,12 @@
+package com.jucaicedoa.appsbasicas1.apps.app4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jucaicedoa.appsbasicas1.R
+
+class App4ListadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app4_listado)
+    }
+}
